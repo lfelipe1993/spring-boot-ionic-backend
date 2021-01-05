6a25ad0a6ba2cb686b7fbc4f8c740dca0556b007
Luiz Felipe <lfelipe1993@live.com>
2021-01-05 14:33:13 -0300

    Testando a primeira classe dominio

--- src/main/java/br/com/digitalzone/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/digitalzone/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package br.com.digitalzone.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.digitalzone.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST OK!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listar = new ArrayList<>();
+		
+		listar.add(cat1);
+		listar.add(cat2);
+		
+		return listar;
+		
 	}
 
 }
