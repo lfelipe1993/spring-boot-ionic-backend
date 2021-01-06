f6def05f1a77f36cad42f97e3962983aa73a66b0
Luiz Felipe <lfelipe1993@live.com>
2021-01-06 19:35:47 -0300

    Estado e cidade

---
+++ src/main/java/br/com/digitalzone/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package br.com.digitalzone.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.digitalzone.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+	
+}
