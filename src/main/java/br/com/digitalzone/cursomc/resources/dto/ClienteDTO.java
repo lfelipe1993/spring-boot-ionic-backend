19bbd9fba123613eae44e966c403376d146ce06d
Luiz Felipe <lfelipe1993@live.com>
2021-01-14 14:36:08 -0300

    PUT,GET,DELETE para cliente

---
+++ src/main/java/br/com/digitalzone/cursomc/resources/dto/ClienteDTO.java
@@ -0,0 +1,61 @@
+package br.com.digitalzone.cursomc.resources.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.digitalzone.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
+	private String email;
+	
+	public ClienteDTO() {
+	}
+	
+	public ClienteDTO(Cliente cli) {
+		id = cli.getId();
+		nome = cli.getNome();
+		email = cli.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/br/com/digitalzone/cursomc/services/ClienteService.java
+++ src/main/java/br/com/digitalzone/cursomc/services/ClienteService.java
@@ -1,24 +1,72 @@
 package br.com.digitalzone.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.digitalzone.cursomc.domain.Cliente;
 import br.com.digitalzone.cursomc.repositories.ClienteRepository;
+import br.com.digitalzone.cursomc.resources.dto.ClienteDTO;
+import br.com.digitalzone.cursomc.services.exceptions.DataIntegrityException;
 import br.com.digitalzone.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repo;
-	
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Cliente não encontrado! id: " + id));
+	}
+	
+	public Cliente insert(Cliente cli) {
+		cli.setId(null);
+
+		return repo.save(cli);
+	}
+
+	public Cliente update(Cliente cli) {
+		Cliente newCli = find(cli.getId());
 		
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Cliente não encontrado! id: "
-																+ id));
+		updateData(newCli, cli);
+		
+		return repo.save(newCli);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (org.springframework.dao.DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas!");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO cliDto) {
+		return new Cliente(cliDto.getId(),cliDto.getNome(),cliDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newCli,Cliente cli) {
+		newCli.setNome(cli.getNome());
+		newCli.setEmail(cli.getEmail());
 	}
 }
