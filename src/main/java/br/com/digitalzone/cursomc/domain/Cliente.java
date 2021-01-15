7a8d75e8357637f782f580e76f1132544a9ed247
Luiz Felipe <lfelipe1993@live.com>
2021-01-15 16:55:37 -0300

    Ajustando delecao de cliente e enderecos

--- src/main/java/br/com/digitalzone/cursomc/domain/Cliente.java
+++ src/main/java/br/com/digitalzone/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -31,8 +32,8 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 
-	
-	@OneToMany(mappedBy = "cliente")
+	//cascade all faz com que quando um cliente for deletado os enderecos tb sejam
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
